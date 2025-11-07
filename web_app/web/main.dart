f514a6e5e41b9da16fabde452a7f3b8394982c44
RK Mañago <rbmanago1@up.edu.ph>
2025-11-07 14:10:47 +0800

    setup MVC and boilerplate code

--- web_app/web/main.dart
+++ web_app/web/main.dart
@@ -1,4 +1,6 @@
 import 'package:web/web.dart' as web;
+import 'package:common/common.dart';
+import 'components/components.dart';
 
 void main() {
   final now = DateTime.now();
@@ -7,3 +9,21 @@ void main() {
       'The time is ${now.hour}:${now.minute} '
       'and your Dart web app is running!';
 }
+
+class WebView implements View {
+  final WebViewState _state = WebViewState();
+  late final ViewObserver _observer;
+
+
+  @override
+  void attachObserver(ViewObserver observer) {
+    _observer = observer;
+  }
+
+  @override
+  void start() {
+    // start rendering
+  }
+}
+
+class WebViewState {}
